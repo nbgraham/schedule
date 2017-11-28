63e50a1a18859a7577a680d02adc1a355480669f
Nick Graham <nickbgraham83@gmail.com>
2017-11-28 11:43:02 -0600

    Don't add building options if not on that page

--- src/ATS/Bundle/ScheduleBundle/Resources/public/js/heatmap/building_codes_select.js
+++ src/ATS/Bundle/ScheduleBundle/Resources/public/js/heatmap/building_codes_select.js
@@ -2,6 +2,10 @@ var default_selected = [];
 document.addEventListener("DOMContentLoaded", function(event) {
 
         var codes_select = document.getElementById("building_codes_select");
+        if (codes_select == null) {
+            return;
+        }
+
         for (code_i in building_codes.sort()) {
             var option = document.createElement("option");
             if (default_selected.indexOf(building_codes[code_i]) >= 0) {
