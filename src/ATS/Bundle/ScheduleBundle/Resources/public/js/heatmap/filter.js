e69d275e4108475fae4b8a01698cf21f1fac5b76
Nick Graham <nickbgraham83@gmail.com>
2017-11-29 11:15:42 -0600

    Store intervals in 2d array like occupancy

--- src/ATS/Bundle/ScheduleBundle/Resources/public/js/heatmap/filter.js
+++ src/ATS/Bundle/ScheduleBundle/Resources/public/js/heatmap/filter.js
@@ -39,12 +39,15 @@ function getOccInDataFormat(buildings, building_codes, max_seat, min_seat, seat,
     let selected_intervals_names = [];
     let rooms = [];
     
-    let start = new moment().day(1).hour(8).minute(0);
+    let start = new moment().day(0);
     while (start.day() < 6) {
-        if (start.hour() < 16 || (start.hour() == 16 && start.minute() == 0)) {
-            intervals.push(new moment(start));    
+        let day = [];
+        start.hour(8).minute(0).add(1,'day');
+        while (start.hour() < 16 || (start.hour() == 16 && start.minute() == 0)) {
+            day.push(new moment(start));  
+            start.add(30, "minutes");                    
         }
-        start.add(30, "minutes");        
+        intervals.push(day);
     }
     
     if (selected_intervals === undefined) {
@@ -61,20 +64,25 @@ function getOccInDataFormat(buildings, building_codes, max_seat, min_seat, seat,
             room_i++;
             rooms.push(building.name + " " + room.number);
             
-            let i_overall_interval = 0;
             let x_count = 0;
             let matrix = seat ? room.seat_occupancy_matrix : room.occupancy_matrix;
             for (let i_day in matrix) {
                 let day_matrix = matrix[i_day];
                 for (let i_interval in day_matrix) {
-                    let interval_name = toShortFormat(intervals[i_overall_interval]);
+                    let interval_name = toShortFormat(intervals[i_day][i_interval]);
                     
                     if (selected_intervals.includes(interval_name)) {
-                        let display_name = intervals[i_overall_interval].format("dd h:mm");
+                        let display_name = intervals[i_day][i_interval].format("dd h:mm");
                         if (! selected_intervals_names.includes(display_name)) {
                             selected_intervals_names.push(display_name); 
                         }
                         let sections = room.interval_names_to_sections_dict[interval_name];
+                        if (sections === undefined) {
+                            console.log(interval_name + " not found in room.");
+                            console.log(room.interval_names_to_sections_dict);
+                            console.log(day_matrix);
+                            console.log("at interval #" + i_interval + " named " + interval_name + " val is " + day_matrix[i_interval])
+                        }
                         x_count++;
                         data.push({
                             y: room_i,
@@ -83,10 +91,7 @@ function getOccInDataFormat(buildings, building_codes, max_seat, min_seat, seat,
                             sections,
                             room_cap: room.seat_count
                         });
-                    } else {
-                        console.debug("Interval not found: " + intervals[i_overall_interval]);
                     }
-                    i_overall_interval++;
                 }
             }
             
